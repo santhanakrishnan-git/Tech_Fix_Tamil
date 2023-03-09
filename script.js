918e59cf9b9d3f9f19dac700787d3ad92122cfa3
Santhana Krishnan <santhanakrishnan182002@gmail.com>
2023-03-09 13:53:31 +0530

    changes

---
+++ script.js
@@ -0,0 +1,17 @@
+
+  
+function icon_fun(y) {
+    var x=document.getElementById("nav-bar");
+    if(x.style.display=="block"){
+        x.style.display="none";
+        y.classList.toggle("change");
+        
+          
+    }else{
+        x.style.display="block";
+        y.classList.toggle("change");
+   
+    }
+    
+    
+  }
