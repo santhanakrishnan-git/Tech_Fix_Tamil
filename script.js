40dd34c26d55259fb918924620be0c5f731de2f1
Santhana Krishnan <santhanakrishnan182002@gmail.com>
2023-03-10 23:04:25 +0530

    changes

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-function myfun() {
-    var z=document.getElementById("id1");
-    if(z.style.display=="block"){
-        z.style.display="none"; 
-    }else{
-        z.style.display="block";
-    }
+function display_fun(z) {
+        z.style.display="block"; 
 }
 function icon_fun(y) {
     var x=document.getElementById("nav-bar");
+    console.log(y)
     if(x.style.display=="block"){
         x.style.display="none";
         y.classList.toggle("change"); 
     }else{
         x.style.display="block";
+        x.style.transition="2s";
         y.classList.toggle("change");
     }
     
