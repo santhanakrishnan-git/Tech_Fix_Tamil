d3dd9f157729409ec0d04a328df09d56c87257b2
Santhana Krishnan <santhanakrishnan182002@gmail.com>
2023-03-12 21:18:08 +0530

    changes

--- script.js
+++ script.js
@@ -3,14 +3,28 @@ function display_fun(z) {
 }
 function icon_fun(y) {
     var x=document.getElementById("nav-bar");
-    console.log(y)
     if(x.style.display=="block"){
         x.style.display="none";
         y.classList.toggle("change"); 
     }else{
         x.style.display="block";
-        x.style.transition="2s";
         y.classList.toggle("change");
     }
     
   }
+  
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
