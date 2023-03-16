724dc7d290f8f666f265961c5fb1e24acb2fb865
Santhana Krishnan <santhanakrishnan182002@gmail.com>
2023-03-16 22:54:59 +0530

    changes

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var a = document.getElementsByClassName("mySlides");
-  for (i = 0; i < a.length; i++) {
-    a[i].style.display = "none";  
-  }
-
-  slideIndex++;
-  if (slideIndex > a.length) {slideIndex = 1;}
-  a[slideIndex-1].style.display = "block";
-  
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
-
 function icon_fun(y) {
     var x=document.getElementById("nav-bar");
     if(x.style.display=="block"){
@@ -27,4 +9,21 @@ function icon_fun(y) {
     }
     
   }
+var slideIndex = 0;
+setTimeout(() => {
+  showSlides();  
+}, 2000);
+
+function showSlides() {
+  var i;
+  var a = document.getElementsByClassName("mySlides");
+  for (i =0; i < a.length; i++) {
+    a[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > a.length) slideIndex = 1;
+  a[slideIndex-1].style.display = "block";
+  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
   
